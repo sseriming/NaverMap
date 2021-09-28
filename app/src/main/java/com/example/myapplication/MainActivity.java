28805ab5015d923dc8bf516047858d4deffb1940
sseriming <sseriming@naver.com>
2021-09-28 16:39:17 +0900

    09월 28일

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapplication;
 
+import android.graphics.Camera;
 import android.graphics.Color;
 import android.graphics.PointF;
 import android.location.Address;
 import android.location.Geocoder;
 import android.nfc.tech.NfcA;
+import android.os.AsyncTask;
 import android.os.strictmode.NonSdkApiUsedViolation;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
@@ -13,11 +15,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.geometry.LatLngBounds;
 import com.naver.maps.map.CameraUpdate;
@@ -34,29 +40,34 @@ import com.naver.maps.map.util.FusedLocationSource;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Executor;
 
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    // 2021-07-08 예은이가 코드를 이상하게 해서 클론하여 다시 시작
 
     Spinner maptype_spinner;
     NaverMap mMap;
     PolygonOverlay mPolygon;
     ArrayList<Marker> mMarkerArrayLIst;
     ArrayList<LatLng> mLatLngList;
+    EditText editText;
+
 
     public  NaverAddrApi naverAddrApi;
     Button btnTest;
     Button btnMove;
-    SearchView searchView;
+    Button btnsearch;
     Marker marker = new Marker();
 
 
@@ -64,12 +75,76 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private FusedLocationSource locationSource;
     private NaverMap naverMap;
 
+    private URL url;
+    private HttpsURLConnection http;
+    public class addressMarker extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+
+
+            String api ="https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query="+strings[0];
+            StringBuilder sb = new StringBuilder();
+            try {
+                url = new URL(api.toString());
+                http = (HttpsURLConnection) url.openConnection();
+                http.setRequestMethod("GET");
+                http.setRequestProperty("Content-Type", "application/josn");
+                http.setRequestProperty("X-NCP-APIGW-API-KEY-ID", "hruw818xqa");
+                http.setRequestProperty("X-NCP-APIGW-API-KEY", "BxzEOvfUCUfMLnPUiggy1iXke77Q9pIgalmqLa8R");
+
+
+                BufferedReader rd;
+                if (http.getResponseCode() >= 200 && http.getResponseCode() <= 300) {
+                    rd = new BufferedReader(new InputStreamReader(http.getInputStream()));
+                } else {
+                    rd = new BufferedReader(new InputStreamReader(http.getErrorStream()));
+                }
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+                rd.close();
+                http.disconnect();
+            } catch (Exception e) {
+                return null;
+            }
+            return sb.toString();
+        }
+
+        protected void onPostExecute(String jsonStr) {
+            JsonParser jsonParser = new JsonParser();
+            double pnuLatitude;
+            double pnuLongitude;
+            JsonObject jsonObj = (JsonObject) jsonParser.parse(jsonStr);
+            JsonArray jsonArray = (JsonArray) jsonObj.get("addresses");
+            jsonObj = (JsonObject) jsonArray.get(0);
+            pnuLatitude =jsonObj.get("x").getAsDouble();
+            pnuLongitude =jsonObj.get("y").getAsDouble();
+
+            Marker markers = new Marker();
+            markers.setPosition(new LatLng(pnuLongitude, pnuLatitude));
+            markers.setMap(naverMap);
+
+        }
+
+        private String makeStringNum(String number) {
+            String strNum="";
+            for (int i=0; i<4-number.length(); i++) {
+                strNum = strNum + "0";
+            }
+            strNum=strNum+number;
+            return strNum;
+        }
+
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchView = findViewById(R.id.sv_location);
 
         FragmentManager fm = getSupportFragmentManager();
         MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
@@ -78,18 +153,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             fm.beginTransaction().add(R.id.map, mapFragment).commit();
         }
         mapFragment.getMapAsync(this);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
 
 
         initViews();
@@ -130,27 +193,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     marker.setMap(naverMap);
                     naverAddrApi = new NaverAddrApi(this);
                     naverAddrApi.execute(new LatLng(coord.latitude, coord.longitude));
-                    //  InfoWindow infoWindow = new InfoWindow();
-                    //  infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(MainActivity.this) {
-                    //       @NonNull
-                    //      @Override
-                    //       public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                    //           return (coord.latitude + ", " + coord.longitude);
-                    //
-                    //  //     }
-                    //    });
-                    //    infoWindow.open(marker);
 
-                    //    mMarkerArrayLIst.add(marker);
-                    //   mLatLngList.add(coord);
-                    //    if (mLatLngList.size() > 2) {
-                    //        mPolygon.setCoords(mLatLngList);
-                    //        mPolygon.setMap(naverMap);
-                    //    }
-                }
-
-
-        );
+           });
 
 
 
@@ -214,6 +258,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         maptype_spinner = findViewById(R.id.maptype_spinner);
         btnTest = findViewById(R.id.btnTest);
         btnMove = findViewById(R.id.btnMove);
+        btnsearch = findViewById(R.id.btnsearch);
+        editText = findViewById(R.id.sample_EditText);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.map_type, R.layout.custom_spinner_item);
         adapter.setDropDownViewResource(R.layout.custom_spinner_item_click);
         maptype_spinner.setAdapter(adapter);
@@ -225,22 +272,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     mMarkerArrayLIst.get(i).setMap(null);
                 }
                 mPolygon.setMap(null);
-                mLatLngList.clear();
+                marker.setMap(null);
             }
         });
-        btnMove.setOnClickListener(new View.OnClickListener(){
+        btnMove.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 CameraPosition cameraPosition = new CameraPosition(
-                        new LatLng(35.962248, 126.680006), 12
+                        new LatLng(35.945255, 126.682155), 11.7
                 );
                 naverMap.setCameraPosition(cameraPosition);
-                naverAddrApi = new NaverAddrApi(naverAddrApi.mMainActivity);
-                naverAddrApi.execute(new LatLng(35.9462369805542, 126.68215506925468));
+
             }
         });
+        btnsearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String address = editText.getText().toString();
+                new addressMarker().execute(address);
 
+            }});
 
-    }
-}
+    }}
